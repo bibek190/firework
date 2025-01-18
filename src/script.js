1a9eb7dbb2f4be355cffefc2c67ed4b0f99af963
bibek190 <bibek9818917036@gmail.com>
2025-01-18 14:32:33 +1100

    particles

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import fireworkVertexShader from "./shaders/firework/vertex.glsl";
+import fireworkFragmentShader from "./shaders/firework/fragment.glsl";
 
 /**
  * Base
@@ -23,12 +25,22 @@ const textureLoader = new THREE.TextureLoader();
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
 };
+sizes.resolution = new THREE.Vector2(
+  sizes.width * sizes.pixelRatio,
+  sizes.height * sizes.pixelRatio
+);
 
 window.addEventListener("resize", () => {
   // Update sizes
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
+  sizes.resolution.set(
+    sizes.width * sizes.pixelRatio,
+    sizes.height * sizes.pixelRatio
+  );
 
   // Update camera
   camera.aspect = sizes.width / sizes.height;
@@ -64,16 +76,44 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setPixelRatio(sizes.pixelRatio);
 
 // Firework
-const createFireWork = () => {
-  const positionArray = 3.0;
-  for (let i = 0; i < sizes.length; i++) {
-    console.log(1);
+const createFireWork = (count, position, size) => {
+  const positionArray = new Float32Array(count * 3);
+
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+    positionArray[i3] = Math.random() - 0.5;
+    positionArray[i3 + 1] = Math.random() - 0.5;
+    positionArray[i3 + 2] = Math.random() - 0.5;
   }
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positionArray, 3)
+  );
+
+  // Material
+
+  const material = new THREE.ShaderMaterial({
+    vertexShader: fireworkVertexShader,
+    fragmentShader: fireworkFragmentShader,
+    uniforms: {
+      uSize: new THREE.Uniform(size),
+      uResolution: new THREE.Uniform(sizes.resolution),
+    },
+  });
+  // Firework
+  const fireWork = new THREE.Points(geometry, material);
+  fireWork.position.copy(position);
+  scene.add(fireWork);
 };
-createFireWork();
+createFireWork(
+  200, //count
+  new THREE.Vector3(), //position
+  0.5 //Size
+);
 /**
  * Test
  */
