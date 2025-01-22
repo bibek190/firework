5ec93fed0db61c24844cb5600cfeaac0e24b5f95
bibek190 <bibek9818917036@gmail.com>
2025-01-22 23:32:42 +1100

    reset

--- src/script.js
+++ src/script.js
@@ -1,139 +1,89 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import GUI from "lil-gui";
-import fireworkVertexShader from "./shaders/firework/vertex.glsl";
-import fireworkFragmentShader from "./shaders/firework/fragment.glsl";
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from 'lil-gui'
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI({ width: 340 });
+const gui = new GUI({ width: 340 })
 
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 // Loaders
-const textureLoader = new THREE.TextureLoader();
+const textureLoader = new THREE.TextureLoader()
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-  pixelRatio: Math.min(window.devicePixelRatio, 2),
-};
-sizes.resolution = new THREE.Vector2(
-  sizes.width * sizes.pixelRatio,
-  sizes.height * sizes.pixelRatio
-);
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
-  sizes.resolution.set(
-    sizes.width * sizes.pixelRatio,
-    sizes.height * sizes.pixelRatio
-  );
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  25,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(1.5, 0, 6);
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1.5, 0, 6)
+scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  antialias: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(sizes.pixelRatio);
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Firework
-const createFireWork = (count, position, size) => {
-  const positionArray = new Float32Array(count * 3);
-
-  for (let i = 0; i < count; i++) {
-    const i3 = i * 3;
-    positionArray[i3] = Math.random() - 0.5;
-    positionArray[i3 + 1] = Math.random() - 0.5;
-    positionArray[i3 + 2] = Math.random() - 0.5;
-  }
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute(
-    "position",
-    new THREE.BufferAttribute(positionArray, 3)
-  );
-
-  // Material
-
-  const material = new THREE.ShaderMaterial({
-    vertexShader: fireworkVertexShader,
-    fragmentShader: fireworkFragmentShader,
-    uniforms: {
-      uSize: new THREE.Uniform(size),
-      uResolution: new THREE.Uniform(sizes.resolution),
-      uTime: new THREE.Uniform(0),
-    },
-  });
-  // Firework
-  const fireWork = new THREE.Points(geometry, material);
-  fireWork.position.copy(position);
-  scene.add(fireWork);
-};
-createFireWork(
-  200, //count
-  new THREE.Vector3(), //position
-  0.5 //Size
-);
 /**
  * Test
  */
+const test = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial()
+)
+scene.add(test)
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  console.log(elapsedTime);
-  // Update controls
-  controls.update();
+const tick = () =>
+{
+    // Update controls
+    controls.update()
 
-  // Render
-  renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera)
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
