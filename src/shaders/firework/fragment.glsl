d230593edc1b750c072530dd9c709e9cf8459496
bibek190 <bibek9818917036@gmail.com>
2025-01-24 14:56:21 +1100

    firework in the sky

--- src/shaders/firework/fragment.glsl
+++ src/shaders/firework/fragment.glsl
@@ -1,12 +1,12 @@
 uniform sampler2D uTexture;
 uniform vec3 uColor;
 
+void main()
+{
+    float textureAlpha = texture(uTexture, gl_PointCoord).r;
 
-void main(){
-    float textureAlpha = texture(uTexture,gl_PointCoord).r;
-    // finalColor
-    gl_FragColor = vec4(uColor,textureAlpha);
-
+    // Final color
+    gl_FragColor = vec4(uColor, textureAlpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
